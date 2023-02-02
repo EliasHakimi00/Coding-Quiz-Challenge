ca06ed53fa0fc2234a8c50717eea7d851c789654
Elias Hakimi <Elias_Hakimi00@outlook.com>
2023-02-02 16:24:36 +0000

    Created the first set of questions, though timer is yet to be implemented, nor have I added a way to track the scores

---
+++ starter/assets/js/logic.js
@@ -0,0 +1,11 @@
+// Query Selectors to select and manipulate HTML elements
+var startquiz = document.getElementById("start");
+var timerElement = document.querySelector(".timer");
+var quizPage = document.getElementById("quizPage");
+
+start.addEventListener("click", function() {
+    startscreen.style.display = "none";
+    quizPage.style.display = "block";
+  });
+
+
