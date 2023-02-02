91494003a00aef48bb74db14cb0b679740bd525b
Elias Hakimi <Elias_Hakimi00@outlook.com>
2023-02-02 16:59:11 +0000

    linked quiz buttons to results page

--- starter/assets/js/logic.js
+++ starter/assets/js/logic.js
@@ -2,10 +2,31 @@
 var startquiz = document.getElementById("start");
 var timerElement = document.querySelector(".timer");
 var quizPage = document.getElementById("quizPage");
+var endscreen = document.getElementById("end-screen");
+var choices = document.getElementById("choices");
+var finalscore = document.getElementById("final-score");
 
-start.addEventListener("click", function() {
+
+let score = 0;
+
+startquiz.addEventListener("click", function() {
     startscreen.style.display = "none";
     quizPage.style.display = "block";
   });
 
 
+// 
+
+choices.addEventListener("click", function(event) {
+  if (event.target.id === "choice1") {
+    score++;
+  }
+
+  quizPage.style.display = "none";
+  endscreen.style.display = "block";
+  finalscore.innerHTML = ` ${score}`;
+});
+
+endscreen.addEventListener("click", function(event) {
+
+})
