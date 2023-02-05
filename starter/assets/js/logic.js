36e9818ee6cef1e1cceaa8af52925279d13ef9a3
Elias Hakimi <Elias_Hakimi00@outlook.com>
2023-02-05 23:57:29 +0000

    Good lord, this is taking forever to figure out. Fixed a bug which was showing the quiz questions on the same page as the start page

--- starter/assets/js/logic.js
+++ starter/assets/js/logic.js
@@ -1,32 +1,79 @@
-// Query Selectors to select and manipulate HTML elements
-var startquiz = document.getElementById("start");
-var timerElement = document.querySelector(".timer");
-var quizPage = document.getElementById("quizPage");
-var endscreen = document.getElementById("end-screen");
-var choices = document.getElementById("choices");
-var finalscore = document.getElementById("final-score");
-
+const startscreen = document.getElementById("startscreen")
+const startButton = document.getElementById("start");
+const quizQuestion = document.getElementById("quizPage");
+const highScores = document.getElementById("high-scores");
+const quizResult = document.getElementById("end-screen");
+const answer1 = document.getElementById("choice1");
+const answer2 = document.getElementById("choice2");
+const answer3 = document.getElementById("choice3");
+const answer4 = document.getElementById("choice4");
+const submitButton = document.getElementById("submit");
+const initialsInput = document.getElementById("initials");
+const scoresList = document.getElementById("scores-list");
 
 let score = 0;
 
-startquiz.addEventListener("click", function() {
+// startButton.addEventListener("click", function() {
+//   quizQuestion.style.display = "block";
+//   highScores.style.display = "none";
+//   score = 0;
+// });
+
+startButton.addEventListener("click", function() {
     startscreen.style.display = "none";
-    quizPage.style.display = "block";
+    quizQuestion.style.display = "block";
   });
 
+answer1.addEventListener("click", function() {
+  score = 1;
+  quizQuestion.style.display = "none";
+  quizResult.style.display = "block";
 
-// 
-
-choices.addEventListener("click", function(event) {
-  if (event.target.id === "choice1") {
-    score++;
-  }
-
-  quizPage.style.display = "none";
-  endscreen.style.display = "block";
-  finalscore.innerHTML = ` ${score}`;
 });
 
-endscreen.addEventListener("click", function(event) {
+answer2.addEventListener("click", function() {
+  score = 0;
+  quizQuestion.style.display = "none";
+  quizResult.style.display = "block";
 
-})
+});
+
+answer3.addEventListener("click", function() {
+  score = 0;
+  quizQuestion.style.display = "none";
+  quizResult.style.display = "block";
+
+});
+
+answer4.addEventListener("click", function() {
+    score = 0;
+    quizQuestion.style.display = "none";
+    quizResult.style.display = "block";
+
+  });
+
+submitButton.addEventListener("click", function(event) {
+  event.preventDefault();
+  const initials = initialsInput.value;
+  const scoreEntry = initials + ": " + score;
+
+  // create a list item to display the score and initials
+  const listItem = document.createElement("li");
+  listItem.textContent = scoreEntry;
+  scoresList.appendChild(listItem);
+
+  // store the score in local storage
+  localStorage.setItem("scoreEntry", scoreEntry);
+
+  quizResult.style.display = "none";
+  highScores.style.display = "block";
+});
+
+window.onload = function() {
+  const scoreEntry = localStorage.getItem("scoreEntry");
+  if (scoreEntry) {
+    const listItem = document.createElement("li");
+    listItem.textContent = scoreEntry;
+    scoresList.appendChild(listItem);
+  }
+};
